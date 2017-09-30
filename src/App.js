ec30f7c191403a691a3633f461beb54059109e9b
dtmkeng <kang081@hotmail.com>
2017-09-30 21:43:04 +0700

    befor diner

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import React, {Component} from 'react';
 import './App.css';
+import 'bulma/css/bulma.css'
 import Webcam from 'react-webcam';
+import Lightbox from 'react-images';
+import Card from './card.js'
 class App extends Component {
   constructor(){
     super();
     this.state={
-      sceen:null
+      sceen:null,
+      image:'./bg.png'
     }
   }
   setRef = (webcam) => {
     this.webcam = webcam;
   }
- 
   capture = () => {
     const imageSrc = this.webcam.getScreenshot();
     console.log(imageSrc)
     this.setState({sceen:imageSrc})
   };
-  onSave=()=>{
-     let a = document.createElement('a');
-     a.href = this.state.sceen;
-     a.download = 'image.png';
-     a.click()
-  }
+
   render() {
+    const avatarStyle ={
+        backgroundImage: "url('./bg.png')"
+    };
     return (
       <div>
+      <div>
+        <h1 className="title1">Stupid Hackathon</h1>
         <Webcam
           audio={false}
-          height={350}
+          height={450} 
+          width={450}
           ref={this.setRef}
           screenshotFormat="image/jpeg"
-          width={350}
         />
-          <button onClick={this.capture}>Capture photo</button>
-          <button onClick={this.onSave}>save</button>
-          {this.state.sceen ? <img src={this.state.sceen} /> : null}
+        <br/>
+          <a href="#data"><button className="button is-primary" onClick={this.capture}>Capture photo</button></a>
+          &nbsp;
+          <br/><br/><br/><br/><br/><br/><br/><br/>
+          <p id="data">{this.state.sceen ?  <div style={{width:450,height:450,marginLeft:460}}>
+             <h3 className="title2">Caption For you </h3>
+             <Card im={this.state.sceen}/>
+             </div> : null} </p>   
+      </div>
+      <br/><br/><br/><br/>
       </div>
     );
   }
---
+++ src/card.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { FacebookButton, FacebookCount } from "react-social";
+export default class Card extends Component {
+    constructor(){
+        super();
+        this.state={
+          sceen:null,
+          image:'./bg.png'
+        }
+      }
+      onSave=()=>{
+        let a = document.createElement('a');
+        a.href = this.state.sceen;
+        a.download = 'image.png';
+        a.click()
+        }
+    render() {
+        
+        let url = "https://github.com";
+        const appId="347460905704240";
+return (
+<div>
+ <div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img  src={this.props.im} alt="Placeholder image"/>
+    </figure>
+  </div>
+    <div className="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br/>
+    </div>
+  </div>
+  <footer className="card-footer">
+    <p className="card-footer-item">
+      <span>
+         <a className='button is-white' href="#">Cancel</a>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <button className="button is-danger" onClick={this.onSave}>save</button>
+      </span>
+    </p>
+    <p className="card-footer-item" >
+    <FacebookButton className="button is-primary" url={url} appId={appId}>
+          Share  on facebook
+    </FacebookButton>
+    </p>
+  </footer>
+</div>
+        )
+    }
+}
